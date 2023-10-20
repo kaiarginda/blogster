b1b3056ab22e55d415bed8ddcf02cd9d598979e0
tornike <tornike.tanda@gmail.com>
2023-10-20 10:46:22 -0700

    FIXED ErrorF

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    test: /\.node$/,
+    use: 'file-loader',
+  },
 
-module.exports = nextConfig
+module.exports = nextConfig;
