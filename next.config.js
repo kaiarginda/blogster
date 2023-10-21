f0473a58a9228deeb904704c8322da12cadd4bc0
tornike <tornike.tanda@gmail.com>
2023-10-20 22:07:53 -0700

    FasdasF

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
-/**  @type {import('next').NextConfig} */
-const nextConfig = {};
+// // /**  @type {import('next').NextConfig} */
+// // const nextConfig = {};
 
-module.exports = nextConfig;
+// // module.exports = nextConfig;
+// module.exports = {
+//   webpack: (
+//     config,
+//     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+//   ) => {
+//     // Important: return the modified config
+//     return config;
+//   },
+// };
+// Example config for adding a loader that depends on babel-loader
+// This source was taken from the @next/mdx plugin source:
+// https://github.com/vercel/next.js/tree/canary/packages/next-mdx
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@mdx-js/loader",
+          options: pluginOptions.options,
+        },
+      ],
+    });
+
+    return config;
+  },
+};
