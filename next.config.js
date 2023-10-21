3a28d915743745d03075334c149d8e0cfe5a0f59
tornike <tornike.tanda@gmail.com>
2023-10-20 22:38:45 -0700

    sao

--- next.config.js
+++ next.config.js
@@ -1,24 +1,35 @@
-// // Example config for adding a loader that depends on babel-loader
-// // This source was taken from the @next/mdx plugin source:
-// // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
-// module.exports = {
-//   webpack: (config, options) => {
-//     config.module.rules.push({
-//       test: /\.mdx/,
-//       use: [
-//         options.defaultLoaders.babel,
-//         {
-//           loader: "@mdx-js/loader",
-//           options: {},
-//         },
-//       ],
-//     });
+// // // Example config for adding a loader that depends on babel-loader
+// // // This source was taken from the @next/mdx plugin source:
+// // // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
+// // module.exports = {
+// //   webpack: (config, options) => {
+// //     config.module.rules.push({
+// //       test: /\.mdx/,
+// //       use: [
+// //         options.defaultLoaders.babel,
+// //         {
+// //           loader: "@mdx-js/loader",
+// //           options: {},
+// //         },
+// //       ],
+// //     });
 
-//     return config;
-//   },
-// };
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx$/,
-});
+// //     return config;
+// //   },
+// // };
+// const withMDX = require("@next/mdx")({
+//   extension: /\.mdx$/,
+// });
 
-module.exports = withMDX();
+// module.exports = withMDX();
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(win32-x64-msvc\.node)$/, // Adjust the regex pattern to match your specific binary files
+      use: "ignore-loader", // Use a custom loader (ignore-loader) to ignore the binary files
+    });
+
+    return config;
+  },
+};
