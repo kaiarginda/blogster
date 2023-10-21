9a15f6647245bf46a720620a7e0855df7f8c71eb
tornike <tornike.tanda@gmail.com>
2023-10-20 21:26:33 -0700

    FIF

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /* @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: config.assetPrefix,
+  isClient: !isServer,
+  isServer,
+  isDevelopment: dev,
+  future: nextConfig.future || {},
+  experimental: nextConfig.experimental || {},
+};
 
 module.exports = nextConfig;
